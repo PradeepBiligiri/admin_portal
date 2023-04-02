409b485400b15fbee2b6e712e7348814a4528011
PradeepBiligiri <bhadripradeepbiligiri@gmail.com>
2023-04-02 16:56:48 +0530

    sidebar

--- src/Components/sidebar/Sidebar.js
+++ src/Components/sidebar/Sidebar.js
@@ -1,5 +1,17 @@
 import React from "react";
 import "./Sidebar.scss";
+import DashboardCustomizeSharpIcon from "@mui/icons-material/DashboardCustomizeSharp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import ProductionQuantityLimitsOutlinedIcon from "@mui/icons-material/ProductionQuantityLimitsOutlined";
+import ReorderOutlinedIcon from "@mui/icons-material/ReorderOutlined";
+import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
+import QueryStatsOutlinedIcon from "@mui/icons-material/QueryStatsOutlined";
+import NotificationAddOutlinedIcon from "@mui/icons-material/NotificationAddOutlined";
+import SystemSecurityUpdateGoodOutlinedIcon from "@mui/icons-material/SystemSecurityUpdateGoodOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import SettingsSuggestOutlinedIcon from "@mui/icons-material/SettingsSuggestOutlined";
+import PersonPinCircleOutlinedIcon from "@mui/icons-material/PersonPinCircleOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 
 function Sidebar() {
   return (
@@ -10,21 +22,65 @@ function Sidebar() {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <DashboardCustomizeSharpIcon className="icon" />
             <span>Dashbpard</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <span>Dashbpard</span>
+            <PersonOutlinedIcon className="icon" />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashbpard</span>
+            <ProductionQuantityLimitsOutlinedIcon className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashbpard</span>
+            <ReorderOutlinedIcon className="icon" />
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingOutlinedIcon className="icon" />
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <QueryStatsOutlinedIcon className="icon" />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationAddOutlinedIcon className="icon" />
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <SystemSecurityUpdateGoodOutlinedIcon className="icon" />
+            <span>System Health</span>
+          </li>
+          <li>
+            <PsychologyOutlinedIcon className="icon" />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsSuggestOutlinedIcon className="icon" />
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <PersonPinCircleOutlinedIcon className="icon" />
+            <span>Profile</span>
+          </li>
+          <li>
+            <LogoutOutlinedIcon className="icon" />
+            <span>Logout</span>
           </li>
         </ul>
       </div>
-      <div className="bottom">Color option</div>
+      <div className="bottom">
+        <div className="coloroption"></div>
+        <div className="coloroption"></div>
+      </div>
     </div>
   );
 }
